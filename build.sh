98db51262043682ac87795f0efa27105d77b3104
John Hansen <ffad@statoil.com>
2018-09-19 13:07:28 +0200

    docker compose

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-appname=demotest
-docker stop -t 0 "$appname"
-docker rm "$appname"
-docker build -f Dockerfile_prod -t demotest . && docker run -d --name demotest -p 8048:80 "$appname"
+docker-compose -f docker-compose-prod.yml up -d --build
 
